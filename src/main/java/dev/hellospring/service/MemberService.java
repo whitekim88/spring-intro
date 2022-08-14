cd6180d7e23ef1ab3268bd3e0edd653d3fa65607
현성 김 <hs-kim@mantech.co.kr>
2022-08-14 09:46:54 +0900

    first commit

---
+++ src/main/java/dev/hellospring/service/MemberService.java
@@ -0,0 +1,52 @@
+package dev.hellospring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import dev.hellospring.domain.Member;
+import dev.hellospring.repository.MemberRepository;
+
+public class MemberService {
+
+  private final MemberRepository memberRepository;
+
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+
+  /**
+   * 회원가입
+   */
+  public Long join(Member member) {
+    validDuplicationMember(member); // 중복 회원 검증
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  private void validDuplicationMember(Member member) {
+    memberRepository
+        .findByName(member.getName())
+        .ifPresent(
+            m -> {
+              throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+  }
+
+  /**
+   * 전체회원 조회
+   */
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  /**
+   * 아이디로 회원 조회
+   */
+
+  public Optional<Member> findOne(Long memberId){
+    return memberRepository.findById(memberId);
+  }
+
+
+}
